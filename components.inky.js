476eec76bd5429e418a5693b7adf8182828e1c93
Tom Hargrave <tom.hargrave19@gmail.com>
2016-05-31 12:45:23 +0100

    Deals hero component

--- components.inky.js
+++ components.inky.js
@@ -253,5 +253,35 @@ module.exports = {
         </container>
       `;
     }
+  },
+  dealshero: {
+    js: function (element) {      
+      var topImage = element.attr('top-image');
+      var bottomImage = element.attr('bottom-image');
+      var href = element.attr('href');
+      var title = element.attr('title');
+      var text = element.attr('text');
+      
+      return `
+        <row class="collapse ${getElementClasses(element)}">
+          <columns small="12" large="12">
+            <center class="deals-hero">
+              <a href="${href}">
+                <img src="${topImage}">
+                <div class="deals-hero__text-container">
+                  <h6 class="text-center deals-hero__title">
+                    ${title}
+                  </h6>
+                  <p class="text-center deals-hero__text">
+                    ${text}
+                  </p>
+                </div>
+                <img src="${bottomImage}">
+              </a>
+            </center>
+          </columns>
+        </row>
+      `;
+    }
   }
 };
