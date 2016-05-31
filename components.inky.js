96a29f7aca68382305e8e6c00f9bd06487daa8fe
Tom Hargrave <tom.hargrave19@gmail.com>
2016-05-31 11:38:59 +0100

    Vertical alert box component

--- components.inky.js
+++ components.inky.js
@@ -218,5 +218,40 @@ module.exports = {
         </container>
       `;
     }
+  },
+  verticalalertblock: {
+    js: function (element) {      
+      var title = element.attr('title');
+      var text = element.attr('text');
+      var buttonText = element.attr('button-text');
+      var buttonHref = element.attr('button-href');
+      var image = element.attr('image');
+      var mobileImage = element.attr('mobile-image');
+      
+      return `
+        <container class="${getElementClasses(element)}">
+          <row class="collapse collapse-bottom">
+            <columns class="hide-for-large" small="6" large="6">
+              <img src="${mobileImage}">
+            </columns>
+          </row>
+          <row class="collapse collapse-bottom">
+            <columns class="show-for-large" small="12" large="12">
+              <img src="${image}">
+            </columns>
+          </row>
+          <row class="collapse collapse-bottom">
+            <alertinfo 
+              title="${title}"
+              text="${text}"
+              button-text="${buttonText}"
+              button-href="${buttonHref}"
+              mobile-columns="12"
+              desktop-columns="12"
+            ></alertinfo>
+          </row>
+        </container>
+      `;
+    }
   }
 };
