5b01d1f5024575e9b7111cd64d3b6f6c7ad137eb
Tom Hargrave <tom.hargrave19@gmail.com>
2016-06-02 10:22:31 +0100

    Simple and Image footer components

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,14 +15,38 @@ module.exports = function (element) {
   var footerFacebookImage = "http://cdn.ymaservices.com/email/global/footer__facebook.jpg";
   var footerFacebookHref = "https://www.facebook.com/Student-Beans-257062494492133/?fref=ts";
   var footerAddress = "Registered Office: 1 Vincent Square, London, SW1 2PN | Registered as a company in England and Wales, number: 5486885 | VAT Registration Number: 873 0262 33. Copyright 2014 Student Beans all rights reserved.";
+  var image1 = element.attr('image-1');
+  var image2 = element.attr('image-2');
+  var href1 = element.attr('href-1');
+  var href2 = element.attr('href-2');
   
+  var imageSection = ``;
+  if (element.attr('images') == "true") {
+    imageSection = `
+      <spacer size="40"></spacer>
+      <row class="meta collapse">
+        <columns small="12" large="6">
+          <a href="${href1}">
+            <img src="${image1}">
+          </a>
+        </columns>
+        <columns class="show-for-large" small="6" large="6">
+          <a href="${href2}">
+            <img src="${image2}">
+          </a>
+        </columns>
+      </row>
+    `;
+  }
+
   return `
-    <container class="${componentHelper.getElementClasses(element)} footer footer__container">
+    <container class="${componentHelper.getElementClasses(element)} footer footer__container">    
+      ${imageSection}
       <row class="meta">
         <columns small="2" large="2"></columns>
         <columns small="9" large="9">
           <center>
-            <spacer size="40"></spacer>
+            <spacer size="30"></spacer>
             <img src="${footerLogo}">
             <spacer size="30"></spacer>
             <p class="text-center">
