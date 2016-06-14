75252f837794678c6b1a24f42ffdaec10bf8f78f
Tom Hargrave <tom.hargrave19@gmail.com>
2016-06-14 12:03:52 +0100

    Discounts working in all clients

--- src/components/discounts/discountImageBlock.js
+++ src/components/discounts/discountImageBlock.js
@@ -2,28 +2,26 @@ module.exports = function (element) {
   var image = element.attr('image');
   var title = element.attr('title');
   var text = element.attr('text');
+  var keyword = element.attr('keyword');
   var buttonText = element.attr('button-text');
   var discountHref = element.attr('discount-href');
   
   return `
     <container>
       <row>
-        <columns small="12" large="12">
-          <row>
-            <columns small="12" large="6">
-              <a href="${discountHref}">
-                <img width="250" height="250" src="${image}">
-              </a>
-            </columns>
-            <columns small="12" large="6">
-              <discountinfo
-                title="${title}"
-                text="${text}"
-                button-href="${discountHref}"
-                button-text="${buttonText}"
-              ></discountinfo>
-            </columns>
-          </row>
+        <columns small="12" large="6">
+          <a href="${discountHref}">
+            <img width="250" height="250" src="${image}">
+          </a>
+        </columns>
+        <columns small="12" large="6">
+          <discountinfo
+            title="${title}"
+            text="${text}"
+            keyword="${keyword}"
+            button-href="${discountHref}"
+            button-text="${buttonText}"
+          ></discountinfo>
         </columns>
       </row>
     </container>
--- src/components/discounts/discountInfo.js
+++ src/components/discounts/discountInfo.js
@@ -1,30 +1,30 @@
 module.exports = function (element) {  
   var title = element.attr('title');
   var text = element.attr('text');
+  var keyword = element.attr('keyword');
   var buttonText = element.attr('button-text');
   var buttonHref = element.attr('button-href');
   
   return `
-    <spacer size="30"></spacer>
-    <h6 class="text-center discount-image-block__title">
-      ${title}
-    </h6>
-    <spacer size="15"></spacer>
-    <p class="text-center discount-image-block__text">
-      ${text}
-    </p>
-    <spacer size="10"></spacer>
-    <row>
-      <columns small="1" large="1">
-      </columns>
-      <columns small="10" large="10">
+    <div class="discount-info__text-container">
+      <br/>
+      <h6 class="discount-info__keyword text-center">
+        ${keyword}
+      </h6>
+      <h6 class="discount-info__title text-center">
+        ${title}
+      </h6>
+      <br/>
+      <p class="discount-info__text text-center">
+        ${text}
+      </p>
+      <br/>
+      <div class="discount-info__button-container">
         <offerbutton
           text="${buttonText}"
           href="${buttonHref}"
-        ></offerbutton> 
-      </columns>
-      <columns small="1" large="1">
-      </columns>  
-    </row>
+        ></offerbutton>
+      </div>
+    </div>
   `;
 }
